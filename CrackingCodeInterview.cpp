7d71bb99faf2436c055e4b901f8853bb3b8a382b
MorningBlueOfficial <MorningBlueOfficial@users.noreply.github.com>
2021-01-21 18:21:44 +0200

    Overflow of UNSIGNED

--- CrackingCodeInterview.cpp
+++ CrackingCodeInterview.cpp
@@ -832,7 +832,11 @@ bool IsPermutation( std::string str1, std::string str2 )
    // std::cout << IsPermutation( "?123!asZZd-@", "Z?s3!d2-1a@Z" ) << std::endl;
 }
 
+
+std::vector< int > v1{ 1, 2, 3 };
+std::vector< int > v2{ 3, 2, 1, 0 };
+
 int main()
 {
-
+   std::cout << ( ( v1.size() - v2.size() ) == -1 ) << std::endl;
 }
